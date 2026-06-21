2b6cadbfa2d7e54a1c2aae60296955efc9fe9c00
Brandon_Cox <brancox@my365.bellevue.edu>
2026-06-20 23:02:25 -0400

    Add Module 2 assignment files

---
+++ module-2/Brandon Cox assignment 2.2.py
@@ -0,0 +1,22 @@
+# Brandon Cox module 2 assignment 2.2
+# program taken from github debugging guide
+
+from random import randint
+
+def average(li: list) -> float:
+    avg = 0
+    for num in li:
+        avg += num
+    avg /= len(li)
+    return avg
+
+def main():
+    li = []
+    for _ in range(5):
+        li.append(randint(1, 10))
+    
+    avg = average(li)
+    print(avg)
+
+if __name__ == "__main__":
+    main()
