387703be40d15fe03c60525cd9e8001bde5c81a2
Brandon_Cox <brancox@my365.bellevue.edu>
2026-07-12 22:34:17 -0400

    module 5 assignments pushed 7-12-26

--- module-5/forestfiresim.py
+++ module-5/forestfiresim.py
@@ -1,13 +1,11 @@
-#Module 5 assignment 5.2 forest simulator python program.
-#gruop A colabrative project
-#Brandon Cox, Jerrold Icban, Rakesh Shrestha, Christian Thompson
-
 """Forest Fire Sim, modified by Sue Sampson, based on a program by Al Sweigart
 A simulation of wildfires spreading in a forest. Press Ctrl-C to stop.
 Inspired by Nicky Case's Emoji Sim http://ncase.me/simulating/model/
 ** use spaces, not indentation to modify **
 Tags: short, bext, simulation"""
+
 import random, sys, time
+
 try:
     import bext
 except ImportError:
@@ -15,6 +13,7 @@ except ImportError:
     print('can install by following the instructions at')
     print('https://pypi.org/project/Bext/')
     sys.exit()
+
 # Set up the constants:
 WIDTH = 79
 HEIGHT = 22
@@ -24,9 +23,9 @@ FIRE = '@'
 EMPTY = ' '
 
 # (!) Try changing these settings to anything between 0.0 and 1.0:
-INITIAL_TREE_DENSITY = 0.20 # Amount of forest that starts with trees.
-GROW_CHANCE = 0.01 # Chance a blank space turns into a tree.
-FIRE_CHANCE = 0.01 # Chance a tree is hit by lightning & burns.
+INITIAL_TREE_DENSITY = 0.20  # Amount of forest that starts with trees.
+GROW_CHANCE = 0.01  # Chance a blank space turns into a tree.
+FIRE_CHANCE = 0.01  # Chance a tree is hit by lightning & burns.
 
 # (!) Try setting the pause length to 1.0 or 0.0:
 PAUSE_LENGTH = 0.5
@@ -35,18 +34,21 @@ PAUSE_LENGTH = 0.5
 def main():
     forest = createNewForest()
     bext.clear()
-    while True: # Main program loop.
+
+    while True:  # Main program loop.
         displayForest(forest)
-        
+
         # Run a single simulation step:
         nextForest = {'width': forest['width'],
                       'height': forest['height']}
+
         for x in range(forest['width']):
             for y in range(forest['height']):
                 if (x, y) in nextForest:
                     # If we've already set nextForest[(x, y)] on a
                     # previous iteration, just do nothing here:
                     continue
+
                 if ((forest[(x, y)] == EMPTY)
                     and (random.random() <= GROW_CHANCE)):
                     # Grow a tree in this empty space.
@@ -69,7 +71,7 @@ def main():
                     # Just copy the existing object:
                     nextForest[(x, y)] = forest[(x, y)]
         forest = nextForest
-        
+
         time.sleep(PAUSE_LENGTH)
 
 
@@ -79,11 +81,9 @@ def createNewForest():
     for x in range(WIDTH):
         for y in range(HEIGHT):
             if (random.random() * 100) <= INITIAL_TREE_DENSITY:
-                forest[(x, y)] = TREE # Start as a tree.
+                forest[(x, y)] = TREE  # Start as a tree.
             else:
-                forest[(x, y)] = EMPTY # Start as an empty space.
-
-
+                forest[(x, y)] = EMPTY  # Start as an empty space.
     return forest
 
 
@@ -98,13 +98,13 @@ def displayForest(forest):
             elif forest[(x, y)] == FIRE:
                 bext.fg('red')
                 print(FIRE, end='')
-
+          	
             elif forest[(x, y)] == EMPTY:
                 print(EMPTY, end='')
         print()
-    bext.fg('reset') # Use the default font color.
-    print('Grow chance: {}% '.format(GROW_CHANCE * 100), end='')
-    print('Lightning chance: {}% '.format(FIRE_CHANCE * 100), end='')
+    bext.fg('reset')  # Use the default font color.
+    print('Grow chance: {}%  '.format(GROW_CHANCE * 100), end='')
+    print('Lightning chance: {}%  '.format(FIRE_CHANCE * 100), end='')
     print('Press Ctrl-C to quit.')
 
 
@@ -113,4 +113,4 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        sys.exit() # When Ctrl-C is pressed, end the program.
+        sys.exit()  # When Ctrl-C is pressed, end the program.
